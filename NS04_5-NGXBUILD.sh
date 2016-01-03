630e79746250b10270d7c91b336acafe08a05059
hisato <st.work587@necomaneki.org>
2016-01-03 10:59:52 +0900

    NGX BUILD INIT ADD.

--- NS04_5-NGXBUILD.sh
+++ NS04_5-NGXBUILD.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#### CentOS7-default-install.git v1.5.0
+# == lnmp-70 v0.0.1 ==
+source build
 ## Nginx Build on User Directory.
 echo "Start Nginx Build"
 mkdir -p ~/rpm/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}/
@@ -8,14 +9,14 @@ mkdir -p ~/rpm/RPMS/{noarch,x86_64}
 echo "%_topdir $HOME/rpm" >> ~/.rpmmacros
 vim ~/.rpmmacros
 ## Nginx SourceCode Install.
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/SRPMS/n/nginx-1.6.3-7.el7.src.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/SRPMS/n/${NGX_BUILD_SRPM}
 ## Nginx Source Directory.
 cd ~/rpm/SOURCES
 ## Nginx Source Downloads.
-curl -O http://nginx.org/download/nginx-1.9.9.tar.gz
+curl -O http://nginx.org/download/${NGX_BUILD_SOURCE}
 ## Nginx BuildFile Edit.
 ## Nginx Version Edit.
-sed -i "s|1.6.3|1.9.9|g" ~/rpm/SPECS/nginx.spec
+sed -i "s|${NGX_BUILD_VERSION_SRPM}|${NGX_BUILD_VERSION_SOURCE}|g" ~/rpm/SPECS/nginx.spec
 sed -i "102a\export LANG='ja_JP.UTF-8'" ~/rpm/SPECS/nginx.spec
 ##
 echo "SSL / http2 Setup? [y]"
@@ -31,8 +32,8 @@ rpmbuild -bb ~/rpm/SPECS/nginx.spec
 ls -l ~/rpm/RPMS/noarch
 ls -l ~/rpm/RPMS/x86_64
 ## Nginx Install
-sudo rpm -ivh ~/rpm/RPMS/noarch/nginx-filesystem-1.9.9-7.el7.centos.noarch.rpm
-sudo rpm -ivh ~/rpm/RPMS/x86_64/nginx-1.9.9-7.el7.centos.x86_64.rpm
+sudo rpm -ivh ~/rpm/RPMS/noarch/nginx-filesystem-${NGX_BUILD_VERSION_SOURCE}-${NGX_BUILD_VERSION_RE_SRPM}.el7.centos.noarch.rpm
+sudo rpm -ivh ~/rpm/RPMS/x86_64/nginx-${NGX_BUILD_VERSION_SOURCE}-${NGX_BUILD_VERSION_RE_SRPM}.el7.centos.x86_64.rpm
 sudo /usr/sbin/nginx -v
 # SNI Check
 sudo nginx -V 2>&1 | grep SNI
