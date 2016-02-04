45c3db9b93cff9c83c13892249af61ee674441ba
HISATO <hisato@ST-MBPR.local>
2016-02-05 01:43:04 +0900

    concrete5 Setup fix.

--- NGXBUILD.sh
+++ NGXBUILD.sh
@@ -9,19 +9,24 @@ mkdir -p ~/rpm/RPMS/{noarch,x86_64}
 echo "%_topdir $HOME/rpm" >> ~/.rpmmacros
 vim ~/.rpmmacros
 ## Nginx SourceCode Install.
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/SRPMS/n/${NGX_BUILD_SRPM}
+#rpm -ivh http://dl.fedoraproject.org/pub/epel/7/SRPMS/n/${NGX_BUILD_SRPM}
+rpm -ivh http://nginx.org/packages/centos/7/SRPMS/${NGX_BUILD_SRPM}
 ## Nginx Source Directory.
 cd ~/rpm/SOURCES
 ## Nginx Source Downloads.
 curl -O http://nginx.org/download/${NGX_BUILD_SOURCE}
 ## Nginx BuildFile Edit.
 ## Nginx Version Edit.
-sed -i "s|${NGX_BUILD_VERSION_SRPM}|${NGX_BUILD_VERSION_SOURCE}|g" ~/rpm/SPECS/nginx.spec
-sed -i "102a\export LANG='ja_JP.UTF-8'" ~/rpm/SPECS/nginx.spec
+sed -i "s|^Version: ${NGX_BUILD_VERSION_SRPM}|Version: ${NGX_BUILD_VERSION_SOURCE}|g" ~/rpm/SPECS/nginx.spec
+sed -i "82a\BuildRequires: GeoIP-devel" ~/rpm/SPECS/nginx.spec
+sed -i "104a\export LANG='ja_JP.UTF-8'" ~/rpm/SPECS/nginx.spec
+sed -i "121a\        --with-http_geoip_module \\\\" ~/rpm/SPECS/nginx.spec
+sed -i "161a\        --with-http_geoip_module \\\\" ~/rpm/SPECS/nginx.spec
 ##
 echo "SSL / http2 Setup? [y]"
 read ngxhttpv2mod
 if [ ${ngxhttpv2mod} = "y" ] ; then
+sed -i "s|with_spdy|with_v2|g" ~/rpm/SPECS/nginx.spec
 sed -i "s|--with-http_spdy_module|--with-http_v2_module|g" ~/rpm/SPECS/nginx.spec
 fi
 #
@@ -35,6 +40,8 @@ ls -l ~/rpm/RPMS/x86_64
 sudo rpm -ivh ~/rpm/RPMS/noarch/nginx-filesystem-${NGX_BUILD_VERSION_SOURCE}-${NGX_BUILD_VERSION_RE_SRPM}.el7.centos.noarch.rpm
 sudo rpm -ivh ~/rpm/RPMS/x86_64/nginx-${NGX_BUILD_VERSION_SOURCE}-${NGX_BUILD_VERSION_RE_SRPM}.el7.centos.x86_64.rpm
 sudo /usr/sbin/nginx -v
+sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default_conf
+sudo mv /etc/nginx/conf.d/example_ssl.conf /etc/nginx/conf.d/example_ssl_conf
 # SNI Check
 sudo nginx -V 2>&1 | grep SNI
 # TLS SNI support enabled
